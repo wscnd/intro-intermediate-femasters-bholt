28388cdc90205baa06336632ea54953046d26e52
dncsv <72019998+dncsv@users.noreply.github.com>
2020-11-03 14:12:53 -0300

    Revert "no ssr"
    This reverts commit 3e9b2cfd0c59eb83f8d28ca30302d2f188e2d754.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import { Router } from "@reach/router";
-import React from "react";
-import { Provider } from "react-redux";
+import React, { lazy, Suspense } from "react";
 import NavBar from "~/components/NavBar";
 import store from "~/redux/store/";
-import { Details } from "~/pages";
-import { Home } from "~/pages";
+import { Provider } from "react-redux";
+import Spinner from "~/components/Spinner";
 
+const Details = lazy(() => import("~/pages/Details/Details"));
+const Home = lazy(() => import("~/pages/Home/Home"));
 const App = () => {
   return (
     <React.StrictMode>
       <div>
         <Provider store={store}>
           <NavBar />
-          <Router>
-            <Home path="/" />
-            <Details path="/details/:id" />
-          </Router>
+          <Suspense
+            fallback={<Spinner size={50} color={store.getState().theme} />}
+          >
+            <Router>
+              <Home path="/" />
+              <Details path="/details/:id" />
+            </Router>
+          </Suspense>
         </Provider>
       </div>
     </React.StrictMode>
   );
 };
 
-ReactDOM.render(<App />, document.getElementById("root"));
+export default App;
